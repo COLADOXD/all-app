a9fafa50ac7d1bd54b14e9d0743fe9532afff5f0
MateoChacon <xdmateo90@gmail.com>
2023-02-14 08:53:56 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import bgMoviles from "./assets/bg-mobile-dark.jpg"
+import bgDesktop from "./assets/bg-desktop-light.jpg"
+import Light from "./assets/icon-sun.svg"
+</script>
+
+<template>
+  <div class="bg-[url('/assets/bg-desktop-light.jpg')] w-screen">
+    <p>TODO</p>
+    <img :src="Light" alt="">
+  </div>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Overpass", "sans-serif"],
+      },
+      textColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "#3172eb",
+
+        secondary: "#8f94a2",
+
+        terceary: "hsl(216, 12%, 54%)",
+
+        cuarto: "#1b212d",
+
+        quinto: "rgba(20,21,25,255)",
+      }),
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "#151c2e",
+
+        secondary: "#212a46",
+
+        terceary: "#3477f5",
+
+        cuarto: "#8f94a2",
+
+        quinto: "rgba(20,21,25,255)",
+      }),
+    },
+  },
+  plugins: [],
+};
